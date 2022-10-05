db3dde2ec44ea1c023f6034f74b51433ad6e599c
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-10-05 11:45:56 -0300

    chore: add structs project

---
+++ 3 - structs/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	jim.updateName("Jimmy")
+	jim.print()
+}
