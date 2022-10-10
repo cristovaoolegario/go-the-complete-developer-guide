6c50d46ef9195b5b27c76b971de457f187e11632
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-10-10 15:53:20 -0300

    chore: 3rd Assignment - Hard mode Interfaces - Add tests and feature

--- Assignments/3 - hard mode interfaces/main.go
+++ Assignments/3 - hard mode interfaces/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
 
+	fileName, err := GetFileName()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	err = OpenFile(fileName)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+func GetFileName() (string, error) {
+	if len(os.Args) == 1 {
+		return "", errors.New("No file name was provided")
+	}
+
+	return os.Args[1], nil
+}
+
+func OpenFile(fileName string) error {
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		return err
+	}
+
+	io.Copy(os.Stdout, file)
+	return nil
 }
--- Assignments/3 - hard mode interfaces/main_test.go
+++ Assignments/3 - hard mode interfaces/main_test.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 func TestMain(t *testing.T) {
-	t.Run("Should return an error message When there's no file to open", func(t *testing.T) {
 
+	t.Run("Should return an error message When there's no args to program", func(t *testing.T) {
+		oldArgs := os.Args
+		defer func() { os.Args = oldArgs }()
+		os.Args = []string{"cmd"}
+
+		_, err := GetFileName()
+
+		if err.Error() != "No file name was provided" {
+			t.Errorf("Expected to thronw an error!")
+		}
+	})
+
+	t.Run("Should return file name When args has been passed to program", func(t *testing.T) {
+		oldArgs := os.Args
+		defer func() { os.Args = oldArgs }()
+		os.Args = []string{"cmd", "TestFileName"}
+
+		fileName, err := GetFileName()
+
+		if fileName != "TestFileName" && err != nil {
+			t.Errorf("Mismatch file name!")
+		}
+	})
+
+	t.Run("Should return an error message When there's no file to open", func(t *testing.T) {
+		err := OpenFile("NotExistingFile.txt")
+
+		if err.Error() != "open NotExistingFile.txt: no such file or directory" {
+			t.Error("Expected to thronw an error! Got:", err)
+		}
 	})
 }
